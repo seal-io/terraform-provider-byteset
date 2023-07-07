c0a187c32db8b87bc83e0615db90476ce5c5d4ed
thxCode <thxcode0824@gmail.com>
2023-07-07 17:18:22 +0800

    ci: fix failed on pr
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- hack/lib/version.sh
+++ hack/lib/version.sh
@@ -68,11 +68,14 @@ function seal::version::get_version_vars() {
     # specify to dev if the tree is dirty.
     if [[ "${GIT_TREE_STATE:-dirty}" == "dirty" ]]; then
       GIT_VERSION="dev"
-    elif ! [[ "${GIT_VERSION}" =~ ^v([0-9]+)\.([0-9]+)(\.[0-9]+)?(-[0-9A-Za-z.-]+)?(\+[0-9A-Za-z.-]+)?$ ]]; then
-      GIT_VERSION="dev"
     fi
 
     # respect specified version
     GIT_VERSION=${VERSION:-${GIT_VERSION}}
+
+    if ! [[ "${GIT_VERSION}" =~ ^v([0-9]+)\.([0-9]+)(\.[0-9]+)?(-[0-9A-Za-z.-]+)?(\+[0-9A-Za-z.-]+)?$ ]]; then
+      GIT_VERSION="dev"
+    fi
+
   fi
 }
